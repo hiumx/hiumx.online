baa30b6992a7f3d61691ea3d3c2ab785e437d324
hiumx <maixuanhieu250103@gmail.com>
2024-04-27 21:55:42 +0700

    HOTFIX: Update link resume on Drive

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -7,7 +7,7 @@ export default function Introduction() {
             <h1>I'm <span>Hieu Mai Xuan</span></h1>
             <div className='introduction__programming__info'>
                 <h3>Software Engineer | DevOps Engineer</h3>
-                <a href='https://drive.google.com/file/d/18hxjbYtRWTN55Fm1jrd8w2BMUf7jpYBx/view' className='introduction__resume__btn'>My Resume</a>
+                <a href='https://drive.google.com/file/d/1T4-H8dVmD4J6oCc1FB5L_WxDxZzeIYVE/view?usp=drive_link' className='introduction__resume__btn'>My Resume</a>
             </div>
         </div>
     )
